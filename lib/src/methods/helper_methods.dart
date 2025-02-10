094a3c06f37b93745fe3aef4ba1b47de21dd20f9
Thareen-Sajana <thareensajana0@gmail.com>
2025-02-10 21:20:46 +0530

    get direction details from API

--- lib/src/methods/helper_methods.dart
+++ lib/src/methods/helper_methods.dart
@@ -2,7 +2,9 @@ import 'package:client/src/data_provider/app_data.dart';
 import 'package:client/src/globle_variable.dart';
 import 'package:client/src/methods/request_helper.dart';
 import 'package:client/src/models/address.dart';
+import 'package:client/src/models/direction_details.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
 class HelperMethods {
@@ -34,6 +36,27 @@ class HelperMethods {
     }
 
     return placeAddress;
+  }
+
+  static Future<DirectionDetails?> getDirectionDetails(
+      LatLng startPosition, LatLng endPosition) async {
+    String url =
+        'https://maps.googleapis.com/maps/api/directions/json?destination=${endPosition.latitude.toString()},${endPosition.longitude.toString()}&origin=${startPosition.latitude.toString()},${startPosition.longitude.toString()}&key=$mapKey';
+    
+    var response = await RequestHelper.getRequest(url);
+
+    if (response == 'failed') {
+      return null;
+    }
+
+    DirectionDetails directionDetails = DirectionDetails(
+        durationText: response['routes'][0]['legs'][0]['duration']['text'],
+        durationValue: response['routes'][0]['legs'][0]['duration']['value'],
+        distanceText: response['routes'][0]['legs'][0]['distance']['text'],
+        distanceValue: response['routes'][0]['legs'][0]['distance']['value'],
+        encodedPoints: response['routes'][0]['overview_polyline']['points']);
+
+    return directionDetails;
   }
 
 
