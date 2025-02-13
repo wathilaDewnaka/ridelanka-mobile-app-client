9dd3f78a281e2ba3c24f833de41fdf13654cead9
Olitha Samaraweera <olithasamaraweera2002@gmail.com>
2025-02-13 22:09:55 +0530

    Added attendance dashboard

---
+++ lib/src/screens/driver/attendance_dashboard.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AttendancePage());
+}
+
+class AttendancePage extends StatelessWidget {
+  const AttendancePage({super.key});
+}
