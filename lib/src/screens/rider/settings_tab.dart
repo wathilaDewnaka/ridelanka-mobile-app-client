239d658f62a33eb42c08de11a529821b648d369c
ThisuriNethma <thisuri.20230946@iit.ac.lk>
2025-02-22 19:50:49 +0530

    Basic structure of Settings Tab

--- lib/src/screens/rider/settings_tab.dart
+++ lib/src/screens/rider/settings_tab.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-class SettingsTab extends StatelessWidget {
-  const SettingsTab({super.key});
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFF0051ED),
-        title: Text(
+        backgroundColor: const Color(0xFF0051ED),
+        title: const Text(
           'Settings',
           style: TextStyle(
             color: Colors.white,
@@ -18,10 +18,32 @@ class SettingsTab extends StatelessWidget {
         ),
         centerTitle: true,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () {
             Navigator.pop(context);
           },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            ListTile(
+              title: const Text('Change Username'),
+              trailing: const Icon(Icons.settings),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Change Email'),
+              trailing: const Icon(Icons.settings),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Change Phone Number'),
+              trailing: const Icon(Icons.settings),
+              onTap: () {},
+            ),
+          ],
         ),
       ),
     );
