92ec4d20b8858c193d5dd2052d2ef341976a776e
senangaKapuge <senanga.20230044@iit.ac.lk>
2025-02-23 16:47:11 +0530

    Added the app bar and aligned borders and buttons

--- lib/src/screens/driver/vehicle_add.dart
+++ lib/src/screens/driver/vehicle_add.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 
 class VehicleAddScreen extends StatefulWidget {
   @override
-  _VehicleAddScreenState createState() =>
-      _VehicleAddScreenState();
+  _VehicleAddScreenState createState() => _VehicleAddScreenState();
 }
 
 class _VehicleAddScreenState extends State<VehicleAddScreen> {
@@ -26,153 +25,246 @@ class _VehicleAddScreenState extends State<VehicleAddScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Add Vehicle')),
-      body: Stepper(
-        type: StepperType.horizontal,
-        currentStep: _currentStep,
-        onStepContinue: () {
-          if (_currentStep < 1) {
-            setState(() => _currentStep += 1);
-          } else {
-            // Submit data
-            print("Submit all data");
-          }
-        },
-        onStepCancel: () {
-          if (_currentStep > 0) {
-            setState(() => _currentStep -= 1);
-          }
-        },
-        steps: [
-          // Step 1: Vehicle Information
-          Step(
-            title: Text(_currentStep == 0 ? 'Step 1: Vehicle Info' : ""),
-            isActive: _currentStep >= 0,
-            state: _currentStep > 0 ? StepState.complete : StepState.indexed,
-            content: Column(
-              children: [
-                DropdownButtonFormField(
-                  items: ['School', 'Staff']
-                      .map((value) =>
-                          DropdownMenuItem(value: value, child: Text(value)))
-                      .toList(),
-                  onChanged: (value) {},
-                  decoration: InputDecoration(
-                    labelText: 'Service Type',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                DropdownButtonFormField(
-                  items: ['Van', 'Bus']
-                      .map((value) =>
-                          DropdownMenuItem(value: value, child: Text(value)))
-                      .toList(),
-                  onChanged: (value) {},
-                  decoration: InputDecoration(
-                    labelText: 'Vehicle Type',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  controller: modelController,
-                  decoration: InputDecoration(
-                    labelText: 'Vehicle Model and Year',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  controller: licenseController,
-                  decoration: InputDecoration(
-                    labelText: 'Driving License Number',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                DropdownButtonFormField(
-                  items: ['Yes', 'No']
-                      .map((value) =>
-                          DropdownMenuItem(value: value, child: Text(value)))
-                      .toList(),
-                  onChanged: (value) {},
-                  decoration: InputDecoration(
-                    labelText: 'Air Conditioning Availability',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ],
+      backgroundColor: const Color(0xFF0051ED),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            AppBar(
+              backgroundColor: const Color(0xFF0051ED),
+              leading: IconButton(
+                icon:
+                    const Icon(Icons.arrow_back, color: Colors.white, size: 26),
+                onPressed: () {},
+              ),
+              elevation: 0,
             ),
+            const Padding(
+              padding: EdgeInsets.only(top: 17.0),
+              child: Text(
+                "Add Vehicle",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(32),
+            topRight: Radius.circular(32),
           ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 25),
+          child: Theme(
+            data: ThemeData(
+              colorScheme: const ColorScheme.light(
+                primary: Color(0xFF0051ED),
+                surface: Colors.white,
+                shadow: Colors.transparent,
+              ),
+            ),
+            child: Stepper(
+              type: StepperType.horizontal,
+              currentStep: _currentStep,
+              onStepContinue: () {
+                if (_currentStep < 1) {
+                  setState(() => _currentStep += 1);
+                } else {
+                  // Submit data
+                  print("Submit all data");
+                }
+              },
+              onStepCancel: () {
+                if (_currentStep > 0) {
+                  setState(() => _currentStep -= 1);
+                }
+              },
+              controlsBuilder: (BuildContext context, ControlsDetails details) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: details.onStepContinue,
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(double.infinity, 50),
+                          backgroundColor: const Color(0xFF0051ED),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                        ),
+                        child: const Text(
+                          "Next",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                      ElevatedButton(
+                        onPressed: details.onStepCancel,
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(double.infinity, 50),
+                          backgroundColor: const Color(0xFF0051ED),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          ),
+                        ),
+                        child: const Text(
+                          "Back",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              steps: [
+                Step(
+                  title: Text(_currentStep == 0 ? 'Step 1: Vehicle Info' : ""),
+                  isActive: _currentStep >= 0,
+                  state:
+                      _currentStep > 0 ? StepState.complete : StepState.indexed,
+                  content: Column(
+                    children: [
+                      DropdownButtonFormField(
+                        items: ['School', 'Staff']
+                            .map((value) => DropdownMenuItem(
+                                value: value, child: Text(value)))
+                            .toList(),
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          labelText: 'Service Type',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      DropdownButtonFormField(
+                        items: ['Van', 'Bus']
+                            .map((value) => DropdownMenuItem(
+                                value: value, child: Text(value)))
+                            .toList(),
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          labelText: 'Vehicle Type',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextField(
+                        controller: modelController,
+                        decoration: InputDecoration(
+                          labelText: 'Vehicle Model and Year',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextField(
+                        controller: licenseController,
+                        decoration: InputDecoration(
+                          labelText: 'Driving License Number',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      DropdownButtonFormField(
+                        items: ['Yes', 'No']
+                            .map((value) => DropdownMenuItem(
+                                value: value, child: Text(value)))
+                            .toList(),
+                        onChanged: (value) {},
+                        decoration: InputDecoration(
+                          labelText: 'Air Conditioning Availability',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
 
-          // Step 2: Pricing and Background
-          Step(
-            title:
-                Text(_currentStep == 1 ? 'Step 2: Pricing & Background' : ''),
-            isActive: _currentStep >= 1,
-            state: _currentStep == 1 ? StepState.indexed : StepState.complete,
-            content: Column(
-              children: [
-                TextField(
-                  controller: startLocationController,
-                  decoration: InputDecoration(
-                    labelText: 'Start Location',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  controller: endLocationController,
-                  decoration: InputDecoration(
-                    labelText: 'End Location',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  controller: priceController,
-                  decoration: InputDecoration(
-                    labelText: 'Price',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    // Predict price logic
-                  },
-                  child: Text('Predict Price'),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  controller: experienceController,
-                  decoration: InputDecoration(
-                    labelText: 'Work Experience',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  controller: languagesController,
-                  decoration: InputDecoration(
-                    labelText: 'Languages',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  controller: descriptionController,
-                  maxLines: 3,
-                  decoration: InputDecoration(
-                    labelText: 'Route details of the vehicle',
-                    border: OutlineInputBorder(),
+                // Step 2: Pricing and Background
+                Step(
+                  title: Text(
+                      _currentStep == 1 ? 'Step 2: Pricing & Background' : ''),
+                  isActive: _currentStep >= 1,
+                  state: _currentStep == 1
+                      ? StepState.indexed
+                      : StepState.complete,
+                  content: Column(
+                    children: [
+                      TextField(
+                        controller: startLocationController,
+                        decoration: InputDecoration(
+                          labelText: 'Start Location',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextField(
+                        controller: endLocationController,
+                        decoration: InputDecoration(
+                          labelText: 'End Location',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextField(
+                        controller: priceController,
+                        decoration: InputDecoration(
+                          labelText: 'Price',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Predict price logic
+                        },
+                        child: Text('Predict Price'),
+                      ),
+                      SizedBox(height: 16),
+                      TextField(
+                        controller: experienceController,
+                        decoration: InputDecoration(
+                          labelText: 'Work Experience',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextField(
+                        controller: languagesController,
+                        decoration: InputDecoration(
+                          labelText: 'Languages',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextField(
+                        controller: descriptionController,
+                        maxLines: 3,
+                        decoration: InputDecoration(
+                          labelText: 'Route details of the vehicle',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
