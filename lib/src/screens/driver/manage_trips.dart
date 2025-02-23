4575870db1dbf3a5c13f7193a7c168fc99a2ac4f
Thareen-Sajana <thareensajana0@gmail.com>
2025-02-23 18:34:14 +0530

    check permission for get location

--- lib/src/screens/driver/manage_trips.dart
+++ lib/src/screens/driver/manage_trips.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class RidesTab extends StatefulWidget {
   @override
@@ -7,15 +11,34 @@ class RidesTab extends StatefulWidget {
 }
 
 class _RidesTabState extends State<RidesTab> {
+
+  GoogleMapController? mapController;
+  Completer<GoogleMapController> _controller = Completer();
+
+  var geoLocator = Geolocator();
+
+  Future<void> checkPermissions() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, handle appropriately.
+        print('Location permissions are denied');
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are permanently denied, handle appropriately.
+      print('Location permissions are permanently denied');
+      return;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          "Welcome to rides Screen",
-          style: TextStyle(fontSize: 24), // Optional: adjust text size
-        ),
-      ),
-    );
+    return Stack(
+      
+    )
   }
 }
