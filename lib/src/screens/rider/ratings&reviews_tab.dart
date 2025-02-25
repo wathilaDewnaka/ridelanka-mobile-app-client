bb98aa3884991a61ba2a8a30f589ffcd9cceb047
ThisuriNethma <thisuri.20230946@iit.ac.lk>
2025-02-25 06:48:15 +0530

    Complete view of the total rating indicator- Rating page

--- lib/src/screens/rider/ratings&reviews_tab.dart
+++ lib/src/screens/rider/ratings&reviews_tab.dart
@@ -40,29 +40,19 @@ class _ReviewsRatingsState extends State<ReviewsRatings> {
               SizedBox(height: 10),
               Row(
                 children: [
-                  Text('4.8', style: Theme.of(context).textTheme.displayLarge),
                   Expanded(
+                      flex: 3,
+                      child: Text('4.8',
+                          style: Theme.of(context).textTheme.displayLarge)),
+                  Expanded(
+                    flex: 7,
                     child: Column(
                       children: [
-                        Row(
-                          children: [
-                            Text('5',
-                                style: Theme.of(context).textTheme.bodyMedium),
-                            Expanded(
-                              child: SizedBox(
-                                width: MediaQuery.of(context).size.width,
-                                child: LinearProgressIndicator(
-                                  value: 0.5,
-                                  minHeight: 25,
-                                  backgroundColor: Colors.grey,
-                                  borderRadius: BorderRadius.circular(7),
-                                  valueColor: const AlwaysStoppedAnimation(
-                                      Color(0xFF0051ED)),
-                                ),
-                              ),
-                            )
-                          ],
-                        )
+                        RatingProgressIndicator(text: '5', value: 1.0),
+                        RatingProgressIndicator(text: '4', value: 0.8),
+                        RatingProgressIndicator(text: '3', value: 0.6),
+                        RatingProgressIndicator(text: '2', value: 0.4),
+                        RatingProgressIndicator(text: '1', value: 0.2),
                       ],
                     ),
                   )
@@ -72,6 +62,42 @@ class _ReviewsRatingsState extends State<ReviewsRatings> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class RatingProgressIndicator extends StatelessWidget {
+  const RatingProgressIndicator({
+    super.key,
+    required this.text,
+    required this.value,
+  });
+
+  final String text;
+  final double value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Text(text, style: Theme.of(context).textTheme.bodyMedium),
+        ),
+        Expanded(
+          flex: 11,
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: LinearProgressIndicator(
+              value: value,
+              minHeight: 11,
+              backgroundColor: Colors.grey,
+              borderRadius: BorderRadius.circular(7),
+              valueColor: const AlwaysStoppedAnimation(Color(0xFF0051ED)),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
