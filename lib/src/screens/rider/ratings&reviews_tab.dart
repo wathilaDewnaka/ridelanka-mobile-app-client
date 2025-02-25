b933f5a1c58be6a6be1eac2b9ede283e7d499893
ThisuriNethma <thisuri.20230946@iit.ac.lk>
2025-02-25 11:33:00 +0530

    Extracted methods in rating page

--- lib/src/screens/rider/ratings&reviews_tab.dart
+++ lib/src/screens/rider/ratings&reviews_tab.dart
@@ -1,3 +1,4 @@
+import 'package:client/src/widgets/overall_progress_indicator.dart';
 import 'package:flutter/material.dart';
 
 class ReviewsRatings extends StatefulWidget {
@@ -38,66 +39,11 @@ class _ReviewsRatingsState extends State<ReviewsRatings> {
               Text(
                   'Ratings and reviews are verified and are from people who use the same type of device that you use'),
               SizedBox(height: 10),
-              Row(
-                children: [
-                  Expanded(
-                      flex: 3,
-                      child: Text('4.8',
-                          style: Theme.of(context).textTheme.displayLarge)),
-                  Expanded(
-                    flex: 7,
-                    child: Column(
-                      children: [
-                        RatingProgressIndicator(text: '5', value: 1.0),
-                        RatingProgressIndicator(text: '4', value: 0.8),
-                        RatingProgressIndicator(text: '3', value: 0.6),
-                        RatingProgressIndicator(text: '2', value: 0.4),
-                        RatingProgressIndicator(text: '1', value: 0.2),
-                      ],
-                    ),
-                  )
-                ],
-              )
+              OverallVehicleRating()
             ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class RatingProgressIndicator extends StatelessWidget {
-  const RatingProgressIndicator({
-    super.key,
-    required this.text,
-    required this.value,
-  });
-
-  final String text;
-  final double value;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          flex: 1,
-          child: Text(text, style: Theme.of(context).textTheme.bodyMedium),
-        ),
-        Expanded(
-          flex: 11,
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            child: LinearProgressIndicator(
-              value: value,
-              minHeight: 11,
-              backgroundColor: Colors.grey,
-              borderRadius: BorderRadius.circular(7),
-              valueColor: const AlwaysStoppedAnimation(Color(0xFF0051ED)),
-            ),
-          ),
-        )
-      ],
     );
   }
 }
