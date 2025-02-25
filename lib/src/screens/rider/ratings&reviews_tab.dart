87357ff3264c57bebfb8ef59f3df7091c9515a98
ThisuriNethma <thisuri.20230946@iit.ac.lk>
2025-02-25 06:16:35 +0530

    Basic view of the total rating indicator

--- lib/src/screens/rider/ratings&reviews_tab.dart
+++ lib/src/screens/rider/ratings&reviews_tab.dart
@@ -29,6 +29,49 @@ class _ReviewsRatingsState extends State<ReviewsRatings> {
           },
         ),
       ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                  'Ratings and reviews are verified and are from people who use the same type of device that you use'),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Text('4.8', style: Theme.of(context).textTheme.displayLarge),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text('5',
+                                style: Theme.of(context).textTheme.bodyMedium),
+                            Expanded(
+                              child: SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                child: LinearProgressIndicator(
+                                  value: 0.5,
+                                  minHeight: 25,
+                                  backgroundColor: Colors.grey,
+                                  borderRadius: BorderRadius.circular(7),
+                                  valueColor: const AlwaysStoppedAnimation(
+                                      Color(0xFF0051ED)),
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
