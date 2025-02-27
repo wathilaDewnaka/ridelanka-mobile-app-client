9333b680541ef0d7a5cf203dd6cd96fde80e9f9c
Thareen-Sajana <thareensajana0@gmail.com>
2025-02-27 19:15:03 +0530

    check permission for get location

---
+++ lib/src/screens/rider/track_vehicle.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class TrackVehicle extends StatefulWidget {
+  const TrackVehicle({super.key});
+
+  @override
+  State<TrackVehicle> createState() => _TrackVehicleState();
+}
+
+class _TrackVehicleState extends State<TrackVehicle> {
+
+  GoogleMapController? mapController;
+  Position? currentPosition;
+
+  Set<Marker> _Markers = {};
+
+  Future<void> checkPermissions() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, handle appropriately.
+        print('Location permissions are denied');
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are permanently denied, handle appropriately.
+      print('Location permissions are permanently denied');
+      return;
+    }
+  }
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
