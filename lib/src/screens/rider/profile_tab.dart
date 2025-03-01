91bd5ca5423e6be96f9d67ff89ca25f18c5168b5
ThisuriNethma <thisuri.20230946@iit.ac.lk>
2025-03-01 12:51:48 +0530

    Completed profile tab

--- lib/src/screens/rider/profile_tab.dart
+++ lib/src/screens/rider/profile_tab.dart
@@ -15,18 +15,18 @@ class ProfileTab extends StatefulWidget {
 }
 
 class _ProfileTabState extends State<ProfileTab> {
-  late String fullName;
-  late String email;
-  late String phone;
+  late String fullName = "Mr. NaN NaN";
+  late String email = "";
+  late String phone = "";
+  bool isLoading = true; // Control UI visibility
 
   Future<void> _logout(BuildContext context) async {
     final FirebaseAuth _auth = FirebaseAuth.instance;
 
     await _auth.signOut();
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.remove(
-        "isPassenger"); // Removes the value associated with the given key
-    // Navigate to the login page or perform other actions after logout
+    await prefs.remove("isPassenger");
+
     Navigator.of(context).pushReplacement(
       MaterialPageRoute(builder: (context) => MobileLoginScreen()),
     );
@@ -50,15 +50,18 @@ class _ProfileTabState extends State<ProfileTab> {
         email = userData['email'] ?? '';
         fullName = userData['fullname'] ?? '';
         phone = userData['phone'] ?? '';
+        isLoading = false; // Stop loading once data is fetched
       });
     } else {
       print("User data not found");
+      setState(() {
+        isLoading = false; // Stop loading even if no data is found
+      });
     }
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getUserDetails();
   }
@@ -68,310 +71,219 @@ class _ProfileTabState extends State<ProfileTab> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFF0051ED),
-        title: Row(
-          children: [
-            Container(
-              margin: EdgeInsets.only(right: 10),
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-              ),
-              child: Padding(
-                padding: EdgeInsets.all(17),
-                child: Center(
-                  child: Text(
-                    fullName.split(" ")[1][0],
-                    style: TextStyle(
-                      fontSize: 55,
-                      color: Color(0xFF0051ED),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        title: isLoading
+            ? null // Show nothing while loading
+            : Row(
                 children: [
-                  Text(
-                    'Hello,',
-                    style: TextStyle(
+                  Container(
+                    margin: EdgeInsets.only(right: 10),
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
                       color: Colors.white,
-                      fontSize: 16,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(17),
+                      child: Center(
+                        child: Text(
+                          fullName.split(" ")[1][0],
+                          style: TextStyle(
+                            fontSize: 55,
+                            color: Color(0xFF0051ED),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                  Text(
-                    fullName,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Hello,',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(
+                          fullName,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          softWrap: false,
+                        ),
+                      ],
                     ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    softWrap: false,
                   ),
                 ],
+              ),
+        toolbarHeight: 130,
+      ),
+      body: isLoading
+          ? Center(
+              child:
+                  CircularProgressIndicator()) // Show loader while fetching data
+          : ListView(
+              children: [
+                const SizedBox(height: 50),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: Text(
+                    'Account Information',
+                    style: TextStyle(
+                        fontSize: 18,
+                        letterSpacing: 0.5,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                _buildInfoTile(Icons.person, 'Username',
+                    "${fullName.split(" ")[1]} ${fullName.split(" ")[2]}"),
+                _buildInfoTile(Icons.email, 'Email', email),
+                _buildInfoTile(Icons.phone, 'Phone Number', phone),
+                const SizedBox(height: 40),
+                _buildSettingsAndLogoutButtons(),
+              ],
+            ),
+    );
+  }
+
+  Widget _buildInfoTile(IconData icon, String title, String value) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 5,
+            blurRadius: 10,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      padding: EdgeInsets.only(left: 13, bottom: 20, top: 20),
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+      child: Row(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Color(0xFF99C2FF),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            padding: EdgeInsets.all(8),
+            child: Icon(
+              icon,
+              color: Color(0xFF0051ED),
+            ),
+          ),
+          SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title.toLowerCase(),
+                style: TextStyle(
+                  color: Colors.grey[600],
+                  fontSize: 16,
+                  letterSpacing: 0.5,
+                ),
+              ),
+              Text(
+                value,
+                style: TextStyle(
+                  fontSize: 16,
+                  letterSpacing: 0.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSettingsAndLogoutButtons() {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18),
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SettingsPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF0051ED),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 12),
+                  ),
+                  icon: Icon(Icons.settings, color: Colors.white, size: 26),
+                  label: Text(
+                    'Settings',
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
               ),
             ),
           ],
         ),
-        toolbarHeight: 130,
-      ),
-      body: ListView(
-        children: [
-          const SizedBox(height: 50),
-          Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: Text(
-              'Account Information',
-              style: TextStyle(
-                  fontSize: 18,
-                  letterSpacing: 0.5,
-                  fontWeight: FontWeight.bold),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  spreadRadius: 5,
-                  blurRadius: 10,
-                  offset: Offset(0, 3),
-                ),
-              ],
-            ),
-            padding: EdgeInsets.only(left: 13, bottom: 20, top: 20),
-            margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+        SizedBox(height: 10),
+        GestureDetector(
+          onTap: () {
+            _logout(context);
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xFF99C2FF),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  padding: EdgeInsets.all(8),
-                  child: Icon(
-                    Icons.person,
-                    color: Color(0xFF0051ED), // Icon color
-                  ),
-                ),
-                SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'username',
-                      style: TextStyle(
-                        color: Colors.grey[600],
-                        fontSize: 16,
-                        letterSpacing: 0.5,
-                      ),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 12),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF0051ED),
+                      borderRadius: BorderRadius.circular(12),
                     ),
-                    Text(
-                      fullName.split(" ")[1] + " " + fullName.split(" ")[2],
-                      style: TextStyle(
-                        fontSize: 16,
-                        letterSpacing: 0.5,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  spreadRadius: 5,
-                  blurRadius: 10,
-                  offset: Offset(0, 3),
-                ),
-              ],
-            ),
-            padding: EdgeInsets.only(left: 13, bottom: 20, top: 20),
-            margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
-            child: Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xFF99C2FF),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  padding: EdgeInsets.all(8),
-                  child: Icon(
-                    Icons.email,
-                    color: Color(0xFF0051ED),
-                  ),
-                ),
-                SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'email',
-                      style: TextStyle(
-                        color: Colors.grey[600],
-                        fontSize: 16,
-                        letterSpacing: 0.5,
-                      ),
-                    ),
-                    Text(
-                      email,
-                      style: TextStyle(
-                        fontSize: 16,
-                        letterSpacing: 0.5,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  spreadRadius: 5,
-                  blurRadius: 10,
-                  offset: Offset(0, 3),
-                ),
-              ],
-            ),
-            padding: EdgeInsets.only(left: 13, bottom: 20, top: 20),
-            margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
-            child: Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xFF99C2FF),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  padding: EdgeInsets.all(8),
-                  child: Icon(
-                    Icons.phone,
-                    color: Color(0xFF0051ED),
-                  ),
-                ),
-                SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'phone number',
-                      style: TextStyle(
-                        color: Colors.grey[600],
-                        fontSize: 16,
-                        letterSpacing: 0.5,
-                      ),
-                    ),
-                    Text(
-                      phone,
-                      style: TextStyle(
-                        fontSize: 16,
-                        letterSpacing: 0.5,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 40),
-          Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 18),
-                      child: ElevatedButton.icon(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SettingsPage()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFF0051ED),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(6),
-                          ),
-                          padding: EdgeInsets.symmetric(vertical: 12),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.logout,
+                          size: 26,
+                          color: Colors.white,
                         ),
-                        icon:
-                            Icon(Icons.settings, color: Colors.white, size: 26),
-                        label: Text(
-                          'Settings',
-                          style: TextStyle(color: Colors.white, fontSize: 16),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              GestureDetector(
-                onTap: () {
-                  _logout(context);
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 18),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Container(
-                          padding: EdgeInsets.symmetric(vertical: 12),
-                          decoration: BoxDecoration(
-                            color: Color(0xFF0051ED),
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.logout,
-                                size: 26,
-                                color: Colors.white,
-                              ),
-                              SizedBox(width: 8),
-                              Text(
-                                'Log Out',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ],
+                        SizedBox(width: 8),
+                        Text(
+                          'Log Out',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              )
-            ],
-          )
-        ],
-      ),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
